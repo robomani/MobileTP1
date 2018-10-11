720eaf9d4b56c58e5f3db445e65350eeb49c75af
Alexandre Mailly Legare <robomani@hotmail.com>
2018-10-11 17:06:12 -0400

    start

---
+++ app/src/main/java/com/example/a1810078/minesweeper/MineSweeper.java
@@ -0,0 +1,13 @@
+package com.example.a1810078.minesweeper;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MineSweeper extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mine_sweeper);
+    }
+}
